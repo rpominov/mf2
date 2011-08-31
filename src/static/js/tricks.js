a06cb1dc9b9ff09801769a7e49053b490d2cc302
Pozadi <meshokmuki@gmail.com>
2011-08-31 22:42:18 +0400

    ...

--- src/static/js/tricks.js
+++ src/static/js/tricks.js
@@ -8,6 +8,12 @@ $(function(){
 	$('.tooltip-contener.on-click').live('mouseout', function(){ $(this).removeClass('hover') })
 	*/
 	
+	function set_sizes() {
+		$('.center > .padding').css('min-height', ($(window).height() - 180) + 'px');
+	}
+	set_sizes();
+	$(window).resize(set_sizes);
+	
 	$('.sidebar .block h3 .text').click(function(){
 		// todo save status in cookies
 		$(this).parent().parent().toggleClass('folded').find('ul').toggle('blind', {}, 200);
